d382b32715ebb869f734c97e25e3a70b6119fbf3
mijsdbwsh1 <casyneu@gmail.com>
2022-04-10 00:59:57 +0200

    Added assets

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import AboutMe from './Assets/AboutMe/AboutMe'
+import Certificates from './Assets/Certificates/Certificates'
+import ContactMe from './Assets/ContactMe/ContactMe'
+import Education from './Assets/Education/Education'
+import Interests from './Assets/Interests/Interests'
+import Langage from './Assets/Language/Language'
+import Projects from './Assets/Projects/Projects'
+import Skills from './Assets/Skills/Skills'
+import WorkExperience from './Assets/WorkExperience/WorkExperience'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className = 'row vh-100'>
+            <AboutMe></AboutMe>
+            </div>
       </header>
     </div>
   );
---
+++ cv/src/Assets/AboutMe/AboutMe.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useRef} from 'react'
+import classes from './AboutMe.module.css'
+
+const AboutMe = (props) => {
+    const AboutMeText = "";
+    const AboutMeHeader = "About Me";
+
+    return (
+        <div className={`col ${classes.AboutMe}`}>
+            test
+        </div>
+    );
+};
+
+export default AboutMe;
