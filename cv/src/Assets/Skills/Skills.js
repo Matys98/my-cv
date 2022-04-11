1459bc23b2fa29f5ec79e8ab7a87c446dd26c790
mijsdbwsh1 <casyneu@gmail.com>
2022-04-11 23:00:00 +0200

    Added Skills vertical tab with skills

--- cv/src/Assets/Skills/Skills.js
+++ cv/src/Assets/Skills/Skills.js
@@ -1,18 +1,89 @@
 import React, {useEffect, useRef} from 'react'
 import classes from './Skills.module.css'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const Skills = (props) => {
 
     const SkillsHeader = "Skills";
 
     return (
-        <h1 className={`col ${classes.Skills}`}>
-            {SkillsHeader}
-            <div className={`col ${classes.SkillsInside}`}>
-                <p1>test</p1>
-            </div>
-        </h1>
-        
+            <h1 className={`col ${classes.Skills}`}>
+                {SkillsHeader}
+                <div className={`col ${classes.SkillsInside}`}>
+                <Tabs>
+                <TabList>
+                <Tab>
+                    <p>Cloud</p>
+                </Tab>
+                <Tab>
+                    <p>Containerization</p>
+                </Tab>
+                <Tab>
+                    <p>IaaC</p>
+                </Tab>
+                <Tab>
+                    <p>CI/CD</p>
+                </Tab>
+                <Tab>
+                    <p>Monitoring</p>
+                </Tab>
+                <Tab>
+                    <p>Data Base</p>
+                </Tab>
+                <Tab>
+                    <p>Programing</p>
+                </Tab>
+                <Tab>
+                    <p>Networking</p>
+                </Tab>
+                </TabList>
+
+                <TabPanel>
+                <div className="panel-content">
+                    <h2>Cloud</h2>
+                    <p>
+                        AWS - Amazon Web Services
+                    </p>
+                </div>
+                </TabPanel>
+                <TabPanel>
+                <div className="panel-content">
+                    <h2>Containerization</h2>
+                </div>
+                </TabPanel>
+                <TabPanel>
+                <div className="panel-content">
+                    <h2>IaaC</h2>
+                </div>
+                </TabPanel>
+                <TabPanel>
+                <div className="panel-content">
+                    <h2>CI/CD</h2>
+                </div>
+                </TabPanel>
+                <TabPanel>
+                <div className="panel-content">
+                    <h2>Monitoring</h2>
+                </div>
+                </TabPanel>
+                <TabPanel>
+                <div className="panel-content">
+                    <h2>Data Base</h2>
+                </div>
+                </TabPanel>
+                <TabPanel>
+                <div className="panel-content">
+                    <h2>Programing</h2>
+                </div>
+                </TabPanel>
+                <TabPanel>
+                <div className="panel-content">
+                    <h2>Networking</h2>
+                </div>
+                </TabPanel>
+            </Tabs>
+                </div>
+            </h1>
     );
 };
 
