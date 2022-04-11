312bbab113b6ab5afa4d5e2fc9c2cb0689464a83
mijsdbwsh1 <casyneu@gmail.com>
2022-04-11 22:43:33 +0200

    Updates

---
+++ precache-manifest.13287f05b91601f27b5e9cd45d94a248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d420856fabb921cff02",
+    "url": "/my-cv/static/css/main.736ea464.chunk.css"
+  },
+  {
+    "revision": "5d420856fabb921cff02",
+    "url": "/my-cv/static/js/main.5d420856.chunk.js"
+  },
+  {
+    "revision": "0b5adef5a8e99730695f",
+    "url": "/my-cv/static/js/1.0b5adef5.chunk.js"
+  },
+  {
+    "revision": "1c8aec5638f22d7f2227",
+    "url": "/my-cv/static/js/2.1c8aec56.chunk.js"
+  },
+  {
+    "revision": "668e03e44cb3690cc43a",
+    "url": "/my-cv/static/js/runtime~main.668e03e4.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-cv/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c8fac3a39fabc283d763aee78867aca1",
+    "url": "/my-cv/index.html"
+  }
+];
